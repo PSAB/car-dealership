c0be8cfcaae3d4dc1f70a60b7abda010855ab7c4
Pavan Sabnis <johnyorangeseed@Pavans-Mac.local>
2020-05-26 11:59:09 -0700

    create insert/view DB functions

--- database.py
+++ database.py
@@ -6,9 +6,32 @@ def create_table():
     # Create a cursor object:
     cur = conn.cursor()
     # Queries go here:
-    cur.execute("CREATE TABLE IF NOT EXISTS Cars(cid INTEGER PRIMARY KEY AUTO_INCREMENT, make VARCHAR(20), model VARCHAR(20)), year INTEGER")
-    cur.execute("INSERT INTO cars VALUES ('Toyota', 'Corolla', 2019), ('Toyota', '4Runner', 2020), ('Toyota', 'Land Cruiser', 2020)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Cars(cid INTEGER PRIMARY KEY AUTOINCREMENT, make TEXT, model TEXT, year INTEGER)")
     # Commit changes
     conn.commit()
     # Close connection
     conn.close()
+
+def insert(make, model, year):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO Cars(make, model, year) VALUES ('{}', '{}', {})".format(make, model, year))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Cars")
+    # fetch the query results:
+    rows = cur.fetchall()
+    conn.commit()
+    conn.close()
+    return rows
+
+create_table()
+insert('Toyota', 'Camry', 2019)
+insert('Toyota', '4Runner', 2019)
+insert('Toyota', 'Camry', 2019)
+insert('Toyota', 'Camry', 2019)
+print(view())
