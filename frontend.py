875ddb32eb5e80bc9c32bf6a79908857c61213b8
Pavan Sabnis <johnyorangeseed@Pavans-Mac.local>
2020-05-25 21:17:52 -0700

    add text parsing

--- frontend.py
+++ frontend.py
@@ -3,12 +3,20 @@ from tkinter import *
 # Create empty window
 window = Tk()
 
+def print_thing():
+    print(e1_value.get())
+
 # Add execute button widget
-b1 = Button(window, text="Execute")
+b1 = Button(window, text="Execute", command = print_thing)
 b1.grid(row = 0, column = 0)
 
 # Add text entry widget
-e1 = Entry(window)
+e1_value = StringVar()
+e1 = Entry(window, textvariable = e1_value)
 e1.grid(row = 0, column = 1)
+
+# Add text widget
+t1 = Text(window, height = 1, width = 20)
+t1.grid(row = 0, column = 2)
 
 window.mainloop()
