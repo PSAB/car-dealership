201d3af96d47efbc54e531160f0ef9a4c2409212
Pavan Sabnis <johnyorangeseed@Pavans-Mac.local>
2020-05-26 16:20:32 -0700

    establish psql connection

--- database.py
+++ database.py
@@ -1,26 +1,27 @@
 import psycopg2
+import credentials
 
 def create_table():
     # Connect to database:
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect(f"dbname={credentials.dbname} user={credentials.user} password={credentials.password} host={credentials.host} port={credentials.port}")
     # Create a cursor object:
     cur = conn.cursor()
     # Queries go here:
-    cur.execute("CREATE TABLE IF NOT EXISTS Cars(cid INTEGER PRIMARY KEY AUTOINCREMENT, make TEXT, model TEXT, year INTEGER)")
+    cur.execute("CREATE TABLE IF NOT EXISTS Cars(cid SERIAL PRIMARY KEY, make VARCHAR(20), model VARCHAR(20), year INTEGER)")
     # Commit changes
     conn.commit()
     # Close connection
     conn.close()
 
 def insert(make, model, year):
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect(f"dbname={credentials.dbname} user={credentials.user} password={credentials.password} host={credentials.host} port={credentials.port}")
     cur = conn.cursor()
     cur.execute("INSERT INTO Cars(make, model, year) VALUES ('{}', '{}', {})".format(make, model, year))
     conn.commit()
     conn.close()
 
 def view():
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect(f"dbname={credentials.dbname} user={credentials.user} password={credentials.password} host={credentials.host} port={credentials.port}")
     cur = conn.cursor()
     cur.execute("SELECT * FROM Cars")
     # fetch the query results:
@@ -34,4 +35,4 @@ insert('Toyota', 'Camry', 2019)
 insert('Toyota', '4Runner', 2019)
 insert('Toyota', 'Camry', 2019)
 insert('Toyota', 'Camry', 2019)
-print(view())
+# print(view())
