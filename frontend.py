43134d5225a557f8e8ffee440444859b087dbacc
Pavan Sabnis <johnyorangeseed@Pavans-Mac.local>
2020-05-25 17:32:19 -0700

    add some widgets

--- frontend.py
+++ frontend.py
@@ -3,7 +3,12 @@ from tkinter import *
 # Create empty window
 window = Tk()
 
+# Add execute button widget
+b1 = Button(window, text="Execute")
+b1.grid(row = 0, column = 0)
 
-
+# Add text entry widget
+e1 = Entry(window)
+e1.grid(row = 0, column = 1)
 
 window.mainloop()
